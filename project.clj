d168b6557f2731adb214bebea4b2450af68b8130
Wonko7 <Wonko7@gmail.com>
2012-07-17 14:39:55 +0200

    first blood

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject first-blood "1.0.0"
+  :plugins [[lein-cljsbuild "0.2.4"]]
+  :cljsbuild {
+    :builds [{
+        ; The path to the top-level ClojureScript source directory:
+        :source-path "src-cljs"
+        ; The standard ClojureScript compiler options:
+        ; (See the ClojureScript compiler documentation for details.)
+        :compiler {
+          :output-to "web/js/main.js"  ; default: main.js in current directory
+          :optimizations :whitespace
+          :pretty-print true}}]})
