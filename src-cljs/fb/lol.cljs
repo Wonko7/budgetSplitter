ab41ca7606167a938ff590c5f5d74cfedabcba18
Wonko7 <Wonko7@gmail.com>
2012-07-22 17:34:15 +0200

    page loading works nicely

--- src-cljs/fb/lol.cljs
+++ src-cljs/fb/lol.cljs
@@ -1,28 +1,25 @@
 (ns fb.lol
   (:use [jayq.core :only [$ inner]]))
 
-;(def currentpage "projects")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;; page loading
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn load-page [name]
   (let [newp ($ (str "#" name))
         newp (if (zero? (.-length newp)) ($ "#404") newp)
-        ;curr ($ (str "#" currentpage))
-        curr ($ "#content")
-        ]
-    (.hide 10 curr)
-    (.empty curr)
-    (.html curr (.html newp))
-    (.show 10 curr)
-    ))
+        curr ($ "#content")]
+    (.hide curr 300 #(-> curr
+                      (.html (.html newp))
+                      (.show 300)))))
 
-($
-  #(jayq.core/delegate ($ "body") "a" :click
-           (fn [e]
-             (let [a    ($ (.-currentTarget e))
-                   link (.attr a "href")]
-               ;(.preventDefault e)
-               ;(js/alert (str "#" link))
-               (load-page link)
-               false)))) 
+($ #(jayq.core/delegate ($ "body") "a" :click
+                        (fn [e]
+                          (let [a    ($ (.-currentTarget e))
+                                link (.attr a "href")]
+                            ;(.preventDefault e)
+                            ;(js/alert (str "#" link))
+                            (load-page link)
+                            false)))) 
 
 ($ #(load-page "projects"))
