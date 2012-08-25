d8a74e28c5c5ef1f2224a838632ff6e5dff95066
Wonko7 <Wonko7@gmail.com>
2012-08-25 13:18:55 +0200

    restrict back pages

--- src-cljs/fb/back-page.cljs
+++ src-cljs/fb/back-page.cljs
@@ -22,7 +22,7 @@
       (def back-pages
         (cons [name {name (replace {["anim" "slideright"] ["anim" "flipleft"]} (name data))}]
               back-end)))) ;; another solution would be to read back anim data and use it on the page to load.
-  (when (and (not= name "rm") (not= name "back"))
+  (when (some #(= name %1) ["proj" "projects" "buddies" "indivbuddy" "total" ])
     (def back-pages (cons [name {name (doall (cons ["anim" "slideright"]
                                                    (map #(vector % (.data a %)) ["pid" "bid" "cid"])))}]
                           (take 15 back-pages)))))
