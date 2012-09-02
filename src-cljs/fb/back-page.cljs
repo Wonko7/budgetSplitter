93d99583c933f39cbb30adc9a31f4c24d058b4a7
Wonko7 <Wonko7@gmail.com>
2012-09-02 19:18:04 +0200

    back pages are bugged.

--- src-cljs/fb/back-page.cljs
+++ src-cljs/fb/back-page.cljs
@@ -23,6 +23,7 @@
         (cons [name {name (replace {["anim" "slideright"] ["anim" "flipleft"]} (name data))}]
               back-end)))) ;; another solution would be to read back anim data and use it on the page to load.
   (when (some #(= name %1) ["proj" "projects" "buddies" "indivbuddy" "total" ])
+    ;(js/console.log "added " name)
     (def back-pages (cons [name {name (doall (cons ["anim" "slideright"]
                                                    (map #(vector % (.data a %)) ["pid" "bid" "cid"])))}]
                           (take 15 back-pages)))))
